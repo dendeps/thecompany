78d32e77c1bfaee33cb2977f217f81ebe0a65a99
Denys Parkhomenko <dendeps@gmail.com>
2022-04-05 14:31:34 +0300

    updated config, added ENV_IS_HEROKU env var

--- config.py
+++ config.py
@@ -17,12 +17,19 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     #SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/dbname[?key=value&key=value...]
-    DATABASE_URL = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+
+    if(os.environ.get('ENV_IS_HEROKU')):
+        DATABASE_URL = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    else:
+        user = os.environ.get('MYSQL_USER')
+        password = os.environ.get('MYSQL_PASSWORD')
+        server = os.environ.get('MYSQL_SERVER')
+        database = os.environ.get('MYSQL_DATABASE')
+        SQLALCHEMY_DATABASE_URI = f'postgresql://{user}:{password}' \
+                                  f'@{server}/{database}'
 
     #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-
 
 
 class TestingConfig(object):
