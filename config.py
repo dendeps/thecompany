14f63190cb0b43bf607cae95a6c599f8aeb12977
Denys Parkhomenko <dendeps@gmail.com>
2022-04-05 16:28:56 +0300

    updated config envIsHeroku

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
 
     #SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/dbname[?key=value&key=value...]
     envIsHeroku = os.environ.get('ENV_IS_HEROKU')
-    if (envIsHeroku == '1'):
+    if envIsHeroku == '1':
         DATABASE_URL = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
         SQLALCHEMY_DATABASE_URI = DATABASE_URL
     else:
